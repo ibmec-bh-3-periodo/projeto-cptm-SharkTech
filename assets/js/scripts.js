114113cdd3b4a8e17359b0e187767a26515f728e
Henrique Costa <hvgcosta@gmail.com>
2025-05-08 16:13:59 -0300

    pequenas-alteracoes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,2 @@
-document.getElementById("nome-form").addEventListener("submit", function (event) {
-    event.preventDefault();
-    const nome = document.getElementById("nome").value;
-    document.getElementById("modal").style.display = "none";
-    document.getElementById("conteudo").textContent = `Bem-vindo(a), ${nome}!`;
-    document.getElementById("conteudo").classList.remove("hidden");
-  });
+
   
