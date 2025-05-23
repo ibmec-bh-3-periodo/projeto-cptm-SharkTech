f018f11aa5ccd87a8ec612aac8df080388877a91
Giovana Marra <giovanamarra.05@gmail.com>
2025-05-23 13:18:05 -0300

    feature/home-saldo-login-semjs

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,49 +1,14 @@
-let mostrando = false;
+const valorSaldo = document.getElementById('valor-saldo');
+const iconeOlho = document.getElementById('icone-olho');
+const toggleBtn = document.getElementById('toggle-saldo');
 
-function toggleSaldo() {
-  const valor = document.getElementById("valor-saldo");
-  const icone = document.getElementById("icone-saldo");
+let saldoVisivel = true;
 
-  mostrando = !mostrando;
-
-  if (mostrando) {
-    valor.textContent = "R$ 30,00";
-    icone.src = "assets/img/eye-open.png";
-  } else {
-    valor.textContent = "R$ ●●●●";
-    icone.src = "assets/img/eye-closed.png";
-  }
-}
-
-
-const estacoes = [
-  "Luz", "Brás", "Palmeiras-Barra Funda", "Osasco", "Jundiaí",
-  "Santo André", "Tamanduateí", "Franco da Rocha", "Ribeirão Pires",
-  "Rio Grande da Serra", "Piqueri", "Vila Olímpia", "Pinheiros"
-];
-
-function mostrarSugestoes() {
-  const input = document.getElementById("search").value.toLowerCase();
-  const sugestoes = document.getElementById("sugestoes");
-
-  sugestoes.innerHTML = "";
-
-  if (input.length === 0) return;
-
-  const filtradas = estacoes.filter(est =>
-    est.toLowerCase().includes(input)
-  );
-
-  filtradas.forEach(est => {
-    const li = document.createElement("li");
-    li.textContent = est;
-    li.onclick = () => {
-      document.getElementById("search").value = est;
-      sugestoes.innerHTML = "";
-    };
-    sugestoes.appendChild(li);
-  });
-}
+toggleBtn.addEventListener('click', () => {
+  saldoVisivel = !saldoVisivel;
+  valorSaldo.textContent = saldoVisivel ? 'R$ 14,70' : '••••';
+  iconeOlho.src = saldoVisivel ? '../assets/img/eye-off.svg' : '../assets/img/eye.svg';
+});
 
 let boasVindas = document.getElementsByClassName("boas-vindas")[0];
 let nomeArmazenado = localStorage.getItem("nomePessoa")
