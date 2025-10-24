bc6b8f37f81c9f2eb97b81755980bc393b9b7408
joaogasparini10 <joaogasparini7@gmail.com>
2025-10-24 10:23:31 -0300

    arrumando sintaxe numero

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,11 @@ import express, {Request, Response}  from 'express';
 
 const server = express();
 
-const WHATSAPP_NUMBER = "11) 99767-7030";
+const WHATSAPP_NUMBER = "5511997677030";
 
 server.get("/fale-conosco", (req: Request, res: Response) => {
     const mensagem = encodeURIComponent("Olá! Gostaria de saber mais sobre os serviços.");
-    const url = `https://wa.me/${0800 055 0121}?text=${mensagem}`;
+    const url = `https://wa.me/${5511997677030}?text=${mensagem}`;
     res.redirect(url);
 });
 
