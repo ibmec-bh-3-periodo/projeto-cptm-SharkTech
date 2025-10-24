041c32388cdebaebdae3b4ee7fc43bfec287bc3d
joaogasparini10 <joaogasparini7@gmail.com>
2025-10-24 10:18:49 -0300

    mudando numero

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,12 @@ import express, {Request, Response}  from 'express';
 
 const server = express();
 
-const WHATSAPP_NUMBER = "0800 055 0121";
+const WHATSAPP_NUMBER = "11) 99767-7030";
 
 server.get("/fale-conosco", (req: Request, res: Response) => {
     const mensagem = encodeURIComponent("Olá! Gostaria de saber mais sobre os serviços.");
     const url = `https://wa.me/${0800 055 0121}?text=${mensagem}`;
+    res.redirect(url);
+});
+
 server.listen(5001)
